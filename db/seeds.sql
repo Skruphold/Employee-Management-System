d3ee8b23bfbecb8a683470c8b852ec0fc0285168
Skruphold <hunterco999@gmail.com>
2022-01-25 19:59:16 -0600

    installed packages created db file as well as schema and seeds built structure for schema

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+
+)
+
+CREATE TABLE employee_role (
+
+)
+
+CREATE TABLE employee_name (
+
+)
