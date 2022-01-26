67db0f5d43a16982780d1b093e2647b17fa674c9
Skruphold <hunterco999@gmail.com>
2022-01-25 22:33:46 -0600

    switched to port 3306

--- utils/connect.js
+++ utils/connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3001,
+    port: 3306,
     user: "root",
     password: "RootRoot1",
     database: "company_db"
